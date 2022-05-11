501d19c8a22fb916c69cb8d465ffe5323c57aaf7
Lasse Stolley <lasse@technikabo.com>
2022-05-11 16:33:53 +0200

    Small changes

--- Sources/MKInvertedCircleOverlay/MKInvertedCircleOverlay.swift
+++ Sources/MKInvertedCircleOverlay/MKInvertedCircleOverlay.swift
@@ -1,21 +1,31 @@
+//
+//  MKInvertedCircleOverlay.swift
+//  MKInvertedCircleOverlay
+//
+//  Created by Lasse Stolley on 11.04.22.
+//  Copyright 2022 Lasse Stolley
+//  MIT License
+//
+//  https://github.com/lassestolley/MKInvertedCircleOverlay
+//
 
-import UIKit
 import MapKit
 
 public class MKInvertedCircleOverlayRenderer: MKOverlayRenderer {
     
+    // MKCircle
     var circle: MKCircle
-    var outColor: UIColor
     
-    public var outColorAlpha: CGFloat = 1.0
+    // Configuration
+    public var outColor: UIColor = .systemBlue
+    public var outAlpha: CGFloat = 1.0
     public var strokeColor: UIColor = .clear
-    public var strokeColorAlpha: CGFloat = 1.0
-    public var strokeWith: CGFloat = 0.0
+    public var strokeAlpha: CGFloat = 1.0
+    public var strokeWidth: CGFloat = 0.0
     
     // Init class
-    public init(circle: MKCircle, outColor: UIColor) {
+    public init(circle: MKCircle) {
         self.circle = circle
-        self.outColor = outColor
         super.init(overlay: circle)
     }
     
@@ -23,13 +33,13 @@ public class MKInvertedCircleOverlayRenderer: MKOverlayRenderer {
     public override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
         let path = UIBezierPath(rect: rect(for: MKMapRect.world))
         let excludePath = UIBezierPath(roundedRect: CGRect(x: circle.coordinate.latitude, y: circle.coordinate.longitude, width: circle.boundingMapRect.size.width, height: circle.boundingMapRect.size.height), cornerRadius: circle.boundingMapRect.size.width)
-        context.setFillColor(outColor.withAlphaComponent(outColorAlpha).cgColor)
+        context.setFillColor(outColor.withAlphaComponent(outAlpha).cgColor)
         path.append(excludePath)
         context.addPath(path.cgPath)
         context.fillPath(using: .evenOdd)
-        context.setLineWidth(strokeWith / zoomScale)
+        context.setLineWidth(strokeWidth / zoomScale)
         context.addPath(excludePath.cgPath)
-        context.setStrokeColor(strokeColor.withAlphaComponent(strokeColorAlpha).cgColor)
+        context.setStrokeColor(strokeColor.withAlphaComponent(strokeAlpha).cgColor)
         context.strokePath()
         context.fillPath()
     }
--- Tests/MKInvertedCircleOverlayTests/MKInvertedCircleOverlayTests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-@testable import MKInvertedCircleOverlay
-
-final class MKInvertedCircleOverlayTests: XCTestCase {
-    func testExample() throws {
-    }
-}
